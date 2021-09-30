0f4e0c6fd4568093ab82ff5b8f195d794e0b2a19
Spandita Banerjee <spanditabanerjee@gmail.com>
2021-09-30 09:44:15 +0530

    js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Competitive Programmer", "Content Creator", "Digital Marketing Manager", "Freelancer"],
+        strings: ["Student", "Developer", "Competitive Programmer", "Content Creator", "Digital Marketer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Competitive Programmer", "Content Creator", "Digital Marketing Manager", "Freelancer"],
+        strings: ["Student", "Developer", "Competitive Programmer", "Content Creator", "Digital Marketer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
